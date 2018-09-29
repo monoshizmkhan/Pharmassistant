30832c6ae127f1b4bde63489b56336bb91ed8d68
monoshizmkhan <jishnukhan@gmail.com>
2018-09-29 11:47:36 +0000

    Delete Medicine.py

--- Classes/Medicine.py
+++
@@ -1,9 +0,0 @@
-class Medicine:
-    def __init__(self, medID, name, type, quantity, price, expiryDate):
-        self.medID=medID
-        self.name=name;
-        self.type=type
-        self.quantity=quantity
-        self.price=price
-        self.expiryDate=expiryDate
-
